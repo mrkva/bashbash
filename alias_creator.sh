c4dddf37f041eb651cc3882ad85e8fd4ad22b170
J. Zeleninsky <mrqwaxgo@gmail.com>
2011-06-05 23:31:01 +0200

    blah no.2

--- alias_creator.sh
+++ alias_creator.sh
@@ -2,6 +2,11 @@
 # Alias creator
 # GNU/GPL3 Jonas Gruska, 2011
 
+# Usage: first add the alias creator to your .bash_profile by
+# 	sh alias_creator.sh alc /PATH/TO/THIS/FILE 
+# Then just add your favourite command like so
+#		alc ALIAS COMMAND
+
 SEQ="alias "$1"='"$2"'"
 echo $SEQ >> ~/.bash_profile
 source ~/.bash_profile
